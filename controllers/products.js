4fbdc90f1131d5eb187cd10077e888a3ecd49843
Sergey Zelenov <mail@zelenov.su>
2019-06-22 16:46:21 +0200

    products by category

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,16 @@
 const Product = require('../models/Product');
 
 module.exports = async function products(ctx, next) {
-  const {categoryId} = ctx.query;
-  ctx.body = await Product.find({$or: [{category: categoryId}, {subcategory: categoryId}]});
+  const {category} = ctx.query;
+  const products = await Product.find({$or: [{category: category}, {subcategory: category}]});
+  
+  ctx.body = {products: products.map(product => ({
+    id: product.id,
+    title: product.title,
+    images: product.images,
+    category: product.category,
+    subcategory: product.subcategory,
+    price: product.price,
+    description: product.description,
+  }))}
 };
